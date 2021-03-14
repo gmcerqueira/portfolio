1b4141f828d896ee7f52c6082e438462ad4fb0d2
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-14 14:01:46 -0300

    Ajuste na execuçao do script do projeto meme generator

--- 05-bonus-meme-generator/script.js
+++ 05-bonus-meme-generator/script.js
@@ -1,28 +1,37 @@
-const textInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
-const insertImg = document.querySelector('#meme-insert');
-const memeImg = document.querySelector('#meme-image');
-const stylesBtn = document.querySelector('#styles-buttons');
-const preDefinedMemes = document.getElementById('pre-defined-memes');
+const textInput = document.querySelector("#text-input");
+const memeText = document.querySelector("#meme-text");
+const insertImg = document.querySelector("#meme-insert");
+const memeImg = document.querySelector("#meme-image");
+const stylesBtn = document.querySelector("#styles-buttons");
+const preDefinedMemes = document.getElementById("pre-defined-memes");
+const imageBorder = document.getElementById("meme-image-container").style;
 
-textInput.addEventListener('input', () => {
+function changeBorder(event) {
+  imageBorder.border = "";
+
+  if (event.target.id === "fire") imageBorder.border = "8px dashed red";
+  if (event.target.id === "water") imageBorder.border = "9px double blue";
+  if (event.target.id === "earth") imageBorder.border = "10px groove green";
+}
+
+function changeMemeText() {
   memeText.innerHTML = textInput.value;
-});
+}
 
-insertImg.addEventListener('change', (event) => {
+function loadMemeImg(event) {
   memeImg.src = URL.createObjectURL(event.target.files[0]);
-});
+}
 
-stylesBtn.addEventListener('click', (event) => {
-  const imageBorder = document.getElementById('meme-image-container').style;
-
-  imageBorder.border = '';
-
-  if (event.target.id === 'fire') imageBorder.border = '8px dashed red';
-  if (event.target.id === 'water') imageBorder.border = '9px double blue';
-  if (event.target.id === 'earth') imageBorder.border = '10px groove green';
-});
-
-preDefinedMemes.addEventListener('click', (event) => {
+function selectPreDefinedMeme(event) {
   memeImg.src = event.target.src;
-});
+}
+
+window.onload = () => {
+  textInput.addEventListener("input", changeMemeText);
+
+  insertImg.addEventListener("change", loadMemeImg);
+
+  stylesBtn.addEventListener("click", changeBorder);
+
+  preDefinedMemes.addEventListener("click", selectPreDefinedMeme);
+};
