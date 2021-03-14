6521ad7a615acd1708b023eee6cb95bd5ed001e2
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-14 15:54:11 -0300

    Ajuste na estilizaçao do projeto color guess

--- 06-bonus-color-guess/script.js
+++ 06-bonus-color-guess/script.js
@@ -1,8 +1,8 @@
-const colorAnswer = document.querySelector('#rgb-color');
-const colorsContainer = document.querySelector('#colors-container');
-const answerText = document.querySelector('#answer');
-const resetBtn = document.querySelector('#reset-game');
-const score = document.querySelector('#score');
+const colorAnswer = document.querySelector("#rgb-color");
+const colorsContainer = document.querySelector("#colors-container");
+const answerText = document.querySelector("#answer");
+const resetBtn = document.querySelector("#reset-game");
+const score = document.querySelector("#score");
 let points = 0;
 
 // Cria cor aleatória
@@ -15,47 +15,60 @@ function randomColor() {
 }
 
 // Verifica se a resposta está correta e marca a pontuação
-function rightGuess(event) {
+function checkGuess(event) {
   const guess = event.target.id;
-  const check = guess === 'rightAnswer';
+  const check = guess === "rightAnswer";
+  answerText.style.color = "black";
   if (check) {
-    answerText.innerHTML = 'Acertou!';
+    answerText.innerHTML = "Right!";
     points += 3;
-    score.innerHTML = points;
-  } else answerText.innerHTML = 'Errou! Tente novamente!';
+    resetColors();
+  } else {
+    answerText.innerHTML = "Wrong! Try again!";
+    if (points > 0) {
+      points -= 1;
+    }
+  }
+  score.innerHTML = points;
 }
 
 // Cria opções de cores
 function createColorBalls(numberOfColors) {
   for (let i = 0; i < numberOfColors; i += 1) {
-    const ball = document.createElement('div');
-    ball.className = 'ball';
+    const ball = document.createElement("div");
+    ball.className = "ball";
     ball.style.backgroundColor = `rgb${randomColor()}`;
-    ball.addEventListener('click', rightGuess);
+    ball.addEventListener("click", checkGuess);
     colorsContainer.appendChild(ball);
   }
 }
 
 // seleciona resposta correta mostra a cor a ser adivinhada
 function selectingAnswer() {
-  const balls = document.querySelectorAll('.ball');
+  const balls = document.querySelectorAll(".ball");
   const colors = [];
   balls.forEach((ball) => colors.push(ball.style.backgroundColor));
   const randomIndex = Math.floor(Math.random() * colors.length);
-  balls[randomIndex].id = 'rightAnswer';
-  colorAnswer.innerHTML = colors[randomIndex].slice(3);
+  balls[randomIndex].id = "rightAnswer";
+  colorAnswer.innerHTML = `Color: ${colors[randomIndex].slice(3)}`;
 }
 
 // Reinicia as cores e resposta correta
-function resetGame() {
-  colorsContainer.innerHTML = '';
+function resetColors() {
+  colorsContainer.innerHTML = "";
   createColorBalls(6);
   selectingAnswer();
-  answerText.innerHTML = 'Escolha uma cor';
+}
+
+function restartGame() {
+  resetColors()
+  answerText.style.color = "transparent";
+  points = 0
+  score.innerHTML = points;
 }
 
 window.onload = () => {
-  createColorBalls(6);
+  resetColors()
   selectingAnswer();
-  resetBtn.addEventListener('click', resetGame);
+  resetBtn.addEventListener("click", restartGame);
 };
