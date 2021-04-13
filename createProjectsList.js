6318fd276c11d5f2838be0408a61c2121b909c15
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-04-13 18:31:36 -0300

    Criando lista de projetos dinamicamente atraves de um objeto, corrençao de lint

---
+++ createProjectsList.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/extensions
+import projects from './projects.js';
+
+const projectsList = document.querySelector('#projects-list');
+
+const createProjectCard = (project) => {
+  const card = document.createElement('a');
+  card.className = 'project_card';
+  card.href = project.path;
+
+  const cardTitle = document.createElement('h3');
+  cardTitle.className = 'project_card-title';
+  cardTitle.innerHTML = project.name;
+  card.appendChild(cardTitle);
+
+  const cardImage = document.createElement('img');
+  cardImage.className = 'project_card-cover';
+  cardImage.src = project.image;
+  cardImage.title = project.description;
+  card.appendChild(cardImage);
+
+  projectsList.appendChild(card);
+  console.log(card);
+};
+
+projects.map((project) => createProjectCard(project));
---
+++ projects.js
@@ -0,0 +1,53 @@
+/* eslint-disable max-len */
+const projects = [
+  {
+    name: 'Lessons Learned',
+    path: './01-lessons-learned/',
+    description: 'Using the main HTML tags to compose the structure of a page, styling and changing the positioning of components through CSS',
+    image: './img/projects_covers/01-lessons-learned.png',
+  },
+  {
+    name: 'Playground Functions',
+    path: './02-playground-functions',
+    description: 'Developing functions following specified requirements for the correct behavior of each one of them.',
+    image: './img/projects_covers/02-playground-functions.png',
+  },
+  {
+    name: 'Pixels Art',
+    path: './03-pixels-art',
+    description: 'Given a color palette and a frame composed of pixels, it allows those who use it to be able to paint whatever they want on the board.',
+    image: './img/projects_covers/03-pixels-art.png',
+  },
+  {
+    name: 'To Do list',
+    path: './04-to-do-list',
+    description: 'Create a list of tasks, being able to select, mark as completed and move each task up or down.',
+    image: './img/projects_covers/04-to-do-list.png',
+  },
+  {
+    name: 'Meme Generator',
+    path: './05-bonus-meme-generator',
+    description: 'Create your own meme using one of the templates provided or your own picture.',
+    image: './img/projects_covers/05-bonus-meme-generator.png',
+  },
+  {
+    name: 'Color Guess',
+    path: './06-bonus-color-guess',
+    description: 'Try to guess the color. ',
+    image: './img/projects_covers/06-bonus-color-guess.png',
+  },
+  {
+    name: 'Mystery Letter',
+    path: './07-bonus-mystery-letter',
+    description: 'Create your own mysterious letter to intrigue your friends',
+    image: './img/projects_covers/07-bonus-mystery-letter.png',
+  },
+  {
+    name: 'Facebook Sign Up Copy',
+    path: './08-facebook-signup',
+    description: 'Copy of the old sing up page from facebook using adaptive styling for different screen sizes.',
+    image: './img/projects_covers/08-facebook-signup.png',
+  },
+];
+
+export default projects;
